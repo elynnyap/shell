ef1cb6f43233bb07a642543d9d97358770ec2aec
E-Lynn Yap <yapelynn@gmail.com>
2017-08-07 18:11:15 -0400

    Basic shell code plus history command (WIP)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,8 +9,11 @@
 // Version 0.4
 //
 
-extern crate getopts;
+extern crate shell;
+extern crate getopts; // Rust library for parsing CLI options
+// https://doc.rust-lang.org/getopts/getopts/index.html
 
+use shell::CircularBuffer;
 use getopts::Options;
 use std::env;
 use std::io::{self, Write};
@@ -18,14 +21,18 @@ use std::process::Command;
 
 struct Shell<'a> {
     cmd_prompt: &'a str,
+    history: CircularBuffer,
 }
 
 impl <'a>Shell<'a> {
     fn new(prompt_str: &'a str) -> Shell<'a> {
-        Shell { cmd_prompt: prompt_str }
+        Shell { 
+            cmd_prompt: prompt_str, 
+            history: CircularBuffer::new(10)
+        }
     }
 
-    fn run(&self) {
+    fn run(&mut self) {
         let stdin = io::stdin();
         let mut stdout = io::stdout();
 
@@ -39,13 +46,27 @@ impl <'a>Shell<'a> {
             let cmd_line = line.trim();
             let program = cmd_line.splitn(1, ' ').nth(0).expect("no program");
 
+            // record the newly-entered command in the history
+            self.history.push(String::from(cmd_line));
+
             match program {
                 ""      =>  { continue; }
                 "exit"  =>  { return; }
+                "history" => { self.print_history(); }
                 _       =>  { self.run_cmdline(cmd_line); }
             }
         }
     }
+
+    // Iterate through the 10 most recently entered commands and print them out
+    fn print_history(&self) {
+        let history_iter = (*self).history.iter();
+
+        for cmd in history_iter {
+            println!("{}", cmd);
+        }
+    }
+    
 
     fn run_cmdline(&self, cmd_line: &str) {
         let argv: Vec<&str> = cmd_line.split(' ').filter_map(|x| {
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub struct CircularBuffer;
+
+impl CircularBuffer {
+    pub fn new(size: u32) -> CircularBuffer {
+        CircularBuffer
+    }
+}
+
